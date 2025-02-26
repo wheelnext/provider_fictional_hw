09b3be37863d1a47a255b9a351666e477360ec98
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-02-25 19:34:54 -0500

    BugFix

--- provider_fictional_hw/__init__.py
+++ provider_fictional_hw/__init__.py
@@ -2,8 +2,4 @@
 
 import importlib.metadata
 
-# from provider_fictional_hw.plugin import FictionalHWPlugin
-
-__all__ = ["FictionalHWPlugin", "__version__"]
-
 __version__ = importlib.metadata.version("provider_fictional_hw")
